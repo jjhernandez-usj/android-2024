629465a3cefa9f7659f00ed28f606885d3af61a6
Juan José Hernández <juanjo.hernandez@movicoders.com>
2024-03-13 16:05:46 +0100

    ActivityA4 implemented

---
+++ app/src/main/java/com/usj/android2024/ActivityA4.kt
@@ -0,0 +1,28 @@
+package com.usj.android2024
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.usj.android2024.databinding.ActivityA4Binding
+import com.usj.android2024.databinding.ActivityABinding
+
+class ActivityA4 : AppCompatActivity() {
+    private val view by lazy { ActivityA4Binding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(view.root)
+        view.login.setOnClickListener {
+            if (!isEmpty() && login() ){
+                val intent = Intent (this,ActivityB::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+    fun isEmpty()  : Boolean{
+        return view.User.text.isEmpty() ||
+        view.Password.text.isEmpty()
+    }
+    fun login() : Boolean {
+        return view.User.text.toString() == view.Password.text.toString()
+    }
+}
