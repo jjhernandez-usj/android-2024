3e297ca4aa59b0bf82869a247f90240b25b16c8c
Juan José Hernández <juanjo.hernandez@movicoders.com>
2024-03-20 16:18:49 +0100

    Added the 3 buttons features to the activity c

--- app/src/main/java/com/usj/android2024/ActivityC4.kt
+++ app/src/main/java/com/usj/android2024/ActivityC4.kt
@@ -14,12 +14,25 @@ class ActivityC4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+
         view.btnCompanyCall.setOnClickListener{
             val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: ${getString(R.string._666666)}"))
             startActivity(intent)
-
         }
 
+        view.btnCompanySend.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.company_email));
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Info");
+            intent.putExtra(Intent.EXTRA_TEXT, "My opinion is...");
+            startActivity(intent)
+        }
+
+        view.btnCompanyOpen.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, )
+            intent.data = Uri.parse(getString(R.string.company_web))
+            startActivity(intent)
+        }
 
 
 
